f2b59a5191098d91bf6cbdb627399e7ceff571a4
Dushyant Joshi <dushyant.joshi@alogic.co>
2020-01-16 23:03:41 +1030

    dd

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,13 +13,14 @@ use Symfony\Component\Routing\Annotation\Route;
  * @IsGranted("ROLE_USER")
  * @package App\Controller
  */
-class AccountController extends AbstractController
+class AccountController extends BaseController
 {
     /**
      * @Route("/account", name="app_account")
      */
-    public function index()
+    public function index(LoggerInterface $logger)
     {
+        $logger->debug('checking user '. $this->getUser()->getEmail());
         return $this->render('account/index.html.twig', [
             'controller_name' => 'AccountController',
         ]);
